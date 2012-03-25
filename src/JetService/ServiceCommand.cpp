914d75479e58f871ee90ffac0177921773fd476a
Eugene Petrenko <eugene.petrenko@gmail.com>
2012-03-25 14:36:07 +0200

    make service access rights know SCM rights

--- src/JetService/ServiceCommand.cpp
+++ src/JetService/ServiceCommand.cpp
@@ -1,17 +1,18 @@
 #include "StdAfx.h"
 #include "ServiceCommand.h"
 
-const SC_Right SC_Right::ALL_ACCESS = SC_Right(L"All access", SERVICE_ALL_ACCESS);
-const SC_Right SC_Right::CHANGE_CONFIG = SC_Right(L"Change config", SERVICE_CHANGE_CONFIG);
-const SC_Right SC_Right::INTERROGATE = SC_Right(L"Interrogate", SERVICE_INTERROGATE);
-const SC_Right SC_Right::QUERY_CONFIG = SC_Right(L"Query config", SERVICE_QUERY_CONFIG);
-const SC_Right SC_Right::START = SC_Right(L"Start", SERVICE_START);
-const SC_Right SC_Right::STOP = SC_Right(L"Stop", SERVICE_STOP);
-const SC_Right SC_Right::USER_DEFINED = SC_Right(L"User defined", SERVICE_USER_DEFINED_CONTROL);
+const SC_Right SC_Right::ALL_ACCESS = SC_Right(L"All access", SCM_Right::ALL_ACCESS, SERVICE_ALL_ACCESS);
+//const SC_Right SC_Right::CHANGE_CONFIG = SC_Right(L"Change config", SERVICE_CHANGE_CONFIG);
+//const SC_Right SC_Right::INTERROGATE = SC_Right(L"Interrogate", SERVICE_INTERROGATE);
+//const SC_Right SC_Right::QUERY_CONFIG = SC_Right(L"Query config", SERVICE_QUERY_CONFIG);
+//const SC_Right SC_Right::START = SC_Right(L"Start", SERVICE_START);
+//const SC_Right SC_Right::STOP = SC_Right(L"Stop", SERVICE_STOP);
+//const SC_Right SC_Right::USER_DEFINED = SC_Right(L"User defined", SERVICE_USER_DEFINED_CONTROL);
 
-ServiceCommand::ServiceCommand(const ServiceSettings* settings, const SCM_Right& right) 
+ServiceCommand::ServiceCommand(const ServiceSettings* settings, const SCM_Right& scmRight, const SC_Right& sRight) 
   : mySettings(settings)
-  , ServiceControlManagerCommand(right)
+  , mySCRight(sRight)
+  , ServiceControlManagerCommand(scmRight)
 {
 }
 
--- src/JetService/ServiceCommand.h
+++ src/JetService/ServiceCommand.h
@@ -5,30 +5,37 @@
 
 class SC_Right {
 private:
+  const SCM_Right& mySCMRight;
   const DWORD myRight;
   const CString myName;
-  SC_Right(CString name, DWORD right) : myRight(right), myName(name) {}
+  SC_Right(CString name, const SCM_Right& scm, DWORD right) 
+    : myRight(right)
+    , mySCMRight(scm)
+    , myName(name) {}
+
 public:
   operator DWORD() const { return myRight; }
   operator CString() const { return myName;}
+  operator const SCM_Right&() const { return mySCMRight;}
   
   static const SC_Right ALL_ACCESS; //SERVICE_ALL_ACCESS
-  static const SC_Right CHANGE_CONFIG; //SERVICE_CHANGE_CONFIG 
-  static const SC_Right INTERROGATE; //SERVICE_INTERROGATE
-  static const SC_Right QUERY_CONFIG; //SERVICE_QUERY_CONFIG
-  static const SC_Right START; //SERVICE_SERVICE_START 
-  static const SC_Right STOP; //SERVICE_SERVICE_STOP
-  static const SC_Right USER_DEFINED; //SERVICE_USER_DEFINED_CONTROL
+  //static const SC_Right CHANGE_CONFIG; //SERVICE_CHANGE_CONFIG 
+  //static const SC_Right INTERROGATE; //SERVICE_INTERROGATE
+  //static const SC_Right QUERY_CONFIG; //SERVICE_QUERY_CONFIG
+  //static const SC_Right START; //SERVICE_SERVICE_START 
+  //static const SC_Right STOP; //SERVICE_SERVICE_STOP
+  //static const SC_Right USER_DEFINED; //SERVICE_USER_DEFINED_CONTROL
 };
 
 
 class ServiceCommand : public ServiceControlManagerCommand
 {
 public:
-  ServiceCommand(const ServiceSettings* settings, const SCM_Right& right);
+  ServiceCommand(const ServiceSettings* settings, const SCM_Right& scmRight, const SC_Right& sRight);
   virtual ~ServiceCommand(void);
 
 private:
   const ServiceSettings* mySettings;
+  const SC_Right& mySCRight;
 };
 

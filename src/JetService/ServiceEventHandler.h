cb536a0fa8b20c33c8c53c8e937d9fe60c7da9c7
Eugene Petrenko <eugene.petrenko@gmail.com>
2012-03-28 18:56:30 +0200

    javadoc

--- src/JetService/ServiceEventHandler.h
+++ src/JetService/ServiceEventHandler.h
@@ -2,6 +2,7 @@
 
 #include "ServiceEventContext.h"
 
+///Implementations of handler must be registered in ServiceMain.cpp
 class ServiceEventHandler
 {
 public:
@@ -11,6 +12,11 @@ public:
 public:
   const CString& GetEventName() const;
   DWORD GetControlEventId() const;
+
+public:
+  ///Implementation of this method should work fast
+  ///If extra work is needed, it should create thread to 
+  ///perform it.
   virtual DWORD HandleServiceEvent(ServiceEventContext* context, DWORD dwControl, DWORD dwEventType, LPVOID lpEventData) = 0;
 
 private:

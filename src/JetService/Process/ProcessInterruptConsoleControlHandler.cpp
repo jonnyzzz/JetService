ed30dd7ce1de3e8a5a59667e00c8d080d293e8ba
Eugene Petrenko <eugene.petrenko@gmail.com>
2012-04-15 16:37:55 +0200

    improve logging

--- src/JetService/Process/ProcessInterruptConsoleControlHandler.cpp
+++ src/JetService/Process/ProcessInterruptConsoleControlHandler.cpp
@@ -24,7 +24,7 @@ void ProcessInterruptConsoleControlHandler::InterruptProcess(PROCESS_INFORMATION
   }
 
   if (myTerminateSent == 0) {
-    LOG.LogInfo(L"Sending CRTL+C to the service process");
+    LOG.LogInfo(L"Sending CRTL+C to the service process");    
     if (0 == GenerateConsoleCtrlEvent(CTRL_C_EVENT, info.dwProcessId)) {
       myCtrlEventFailed = true;
       LOG.LogWarnFormat(L"Failed to stop process with CTRL+C. %s", LOG.GetLastError());
@@ -40,11 +40,17 @@ void ProcessInterruptConsoleControlHandler::InterruptProcess(PROCESS_INFORMATION
     //workaround for 49.7 days running windows
     //http://msdn.microsoft.com/en-us/library/windows/desktop/ms724408(v=vs.85).aspx
     myTerminateSent = nowTicks;
-  } else if (myTerminateSent < nowTicks + mySettings->getTerminateWaitTimeoutMilliseconds()) {
-    //timeout has passed
-    LOG.LogWarn(L"Failed to wait for process to exit by CTRL+C.");
-    myCtrlEventFailed = true;
     return;
-  }
+  } 
+  
+  if (myTerminateSent + mySettings->getTerminateWaitTimeoutMilliseconds() < nowTicks) {
+    //timeout
+    LOG.LogWarn(L"Failed to wait for process to exit by CTRL+C");
+    myCtrlEventFailed = true;    
+    return;
+  } 
+
+
+  LOG.LogDebugFormat(L"Waiting for process to exit by CTRL+C");
 }
 
